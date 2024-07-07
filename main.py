10360490ac08d8d1fe13e6c6260b36b364bb672f
pgv123 <32214613+pgv123@users.noreply.github.com>
2024-07-07 14:46:44 +1000

    ChatGPT tidy up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-
 import aioble
 import bluetooth
 from lora_e32 import Logger, LoRaE32, Configuration
@@ -9,6 +8,7 @@ import uasyncio as asyncio
 from micropython import const
 import struct
 
+#Costants and flags
 _FLAG_READ = const(0x0002)
 _FLAG_WRITE_NO_RESPONSE = const(0x0004)
 _FLAG_WRITE = const(0x0008)
@@ -17,24 +17,39 @@ _FLAG_NOTIFY = const(0x0010)
 #Device Information
 Company = "AusSport" # Limited Chars
 Model = "Pico BLE" # to LORA Interface"
-def uid():
-    """ Return the unique id of the device as a string """
-    return "{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}".format(
-        *machine.unique_id())
+
 Hardware = "1.0"
 Software = "1.0.0.1"
 
 
+#UUIDs
+#the Generic Device Info Service
+_DEVICE_INFO_UUID = bluetooth.UUID(0x180A)
+#Info Service Characteristics
 MANUFACTURER_ID = const(0x02A29)
 MODEL_NUMBER_ID = const(0x2A24)
 SERIAL_NUMBER_ID = const(0x2A25)
 HARDWARE_REVISION_ID = const(0x2A26)
 BLE_VERSION_ID = const(0x2A28)
+#the Generic Battery Service
+_BATTERY_UUID = bluetooth.UUID(0x180F)
+#the Battery Characteristic
+_BATTERY = bluetooth.UUID(0x2A19)
+#the Project Service
+_PROJECT_UUID = bluetooth.UUID("116459e5-ad1a-4d85-9b9d-fc2e6cd6b3e0")
+#the Project Characteristics
+_PROJ_NUM_UUID = bluetooth.UUID("116459e6-ad1a-4d85-9b9d-fc2e6cd6b3e0")
+_PROJ_KEEPALIVE_UUID = bluetooth.UUID("116459e7-ad1a-4d85-9b9d-fc2e6cd6b3e0")
+#this is the generic Nordic Uart Service UUID
+_UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+#the UART characteristics
+_TX_UUID = bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
+_RX_UUID = bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
 
-#button_a = Button(12)
-#button_b = Button(13)
-#button_x = Button(14)
-#button_y = Button(15)
+_BLE_APPEARANCE_GENERIC_REMOTE_CONTROL = const(384)
+
+ADV_INTERVAL_MS = 250_000
+
 
 led = Pin("LED", Pin.OUT)
 
@@ -44,109 +59,67 @@ lora = LoRaE32('433T20D', uart1, m0_pin=21, m1_pin=22)
 code = lora.begin()
 print("Initialization: {}", ResponseStatusCode.get_description(code))
 
-_DEVICE_INFO_UUID = bluetooth.UUID(0x180A) # Device Information
-_GENERIC = bluetooth.UUID(0x1848)
-_BATTERY_UUID = bluetooth.UUID(0x180F)
-_ROBOT = bluetooth.UUID(0x1800)
+#Functions
 
-#the Project Service and Characteristic
-_PROJECT_UUID = bluetooth.UUID("116459e5-ad1a-4d85-9b9d-fc2e6cd6b3e0")
+#  Return the unique id of the device as a string to be used as Device Serial no.
+def uid():
+     return "{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}".format(
+        *machine.unique_id())
 
-_PROJ_NUM_UUID = bluetooth.UUID("116459e6-ad1a-4d85-9b9d-fc2e6cd6b3e0")
-_PROJ_KEEPALIVE_UUID = bluetooth.UUID("116459e7-ad1a-4d85-9b9d-fc2e6cd6b3e0")
-
-#this is the generic Nordic Uart Service UUID, it supports two characteristics - TX and RX
-_UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
-
-#the UART characteristics
-_TX_UUID = bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
-_RX_UUID = bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
-
-#the battery characteristic
-_BATTERY = bluetooth.UUID(0x2A19)
-                              
-_BLE_APPEARANCE_GENERIC_REMOTE_CONTROL = const(384)
-
-ADV_INTERVAL_MS = 250_000
-
-device_info = aioble.Service(_DEVICE_INFO_UUID)
-
-project_info = aioble.Service(_PROJECT_UUID)
-
-
-
-                              
-connection = None
-
-Project = "000000"
-
-
-readproj = False
-while (readproj == False):
-    try:
-        f1 = open('project.txt','r')
-        Project = f1.read()
-        readproj = True
-    except:              #I think this means there is no such file
-        f1 = open('project.txt','w')
-        f1.write('000000')
-        f1.close()
-    
-
-print (f'Project No: {Project}')
-
-proj_characteristic = aioble.Characteristic(project_info, _PROJ_NUM_UUID, read=True, write=True, capture=True, initial=Project)
-keepalive_characteristic = aioble.Characteristic(project_info, _PROJ_KEEPALIVE_UUID, write=True, capture=True, initial="Not OK")
-
-print (f'Project Char: {proj_characteristic}')
-print (f'Keep Alive Char: {proj_characteristic}')
-# Create Characteristic for device info
-aioble.Characteristic(device_info, bluetooth.UUID(MANUFACTURER_ID), read=True, initial=Company)
-aioble.Characteristic(device_info, bluetooth.UUID(MODEL_NUMBER_ID), read=True, initial=Model)
-aioble.Characteristic(device_info, bluetooth.UUID(SERIAL_NUMBER_ID), read=True, initial=uid())
-aioble.Characteristic(device_info, bluetooth.UUID(HARDWARE_REVISION_ID), read=True, initial=Hardware) #sys.version)
-aioble.Characteristic(device_info, bluetooth.UUID(BLE_VERSION_ID), read=True, initial=Software)
-
-
-remote_service = aioble.Service(_GENERIC)
-uart_service = aioble.Service(_UART_UUID)
-
-#button_characteristic = aioble.Characteristic(
- #   remote_service, _BUTTON_UUID, read=True, notify=True)
-
-
-_UART_RX = (    _RX_UUID,    _FLAG_WRITE | _FLAG_WRITE_NO_RESPONSE,  )
-_UART_TX = (    _TX_UUID,    _FLAG_READ | _FLAG_NOTIFY,   )
-
-
-rx_characteristic = aioble.Characteristic(
-    uart_service, _RX_UUID, write=True, notify=True, capture=True)
-
-tx_characteristic = aioble.Characteristic(
-    uart_service, _TX_UUID, read=True, notify=True)
-
-#set up the battery service and characteristic
-battery_info = aioble.Service(_BATTERY_UUID)
-
-batt_level = aioble.Characteristic(battery_info, _BATTERY, read=True, notify=True)
-
-print("Registering services")
-
-aioble.register_services(uart_service, device_info, project_info, battery_info)
-
-print("Out of Registering services")
-connected = False
-
+#Get the battery voltage
 def measure_vsys():
     Pin(25, Pin.OUT, value=1)
     Pin(29, Pin.IN, pull=None)
     reading = ADC(3).read_u16() # * 9.9 / 2**16
     Pin(25, Pin.OUT, value=0, pull=Pin.PULL_DOWN)
     Pin(29, Pin.ALT, pull=Pin.PULL_DOWN, alt=7)
-    return reading
+    return reading                           
 
+# Helper to encode the voltage characteristic encoding (sint16).
+def _encode_voltage(vc):
+    return struct.pack("<h", int(vc))
+
+#Read project number from file or create it
+readproj = False
+while not readproj:
+    try:
+        with open('project.txt','r') as f:
+            Project = f.read().strip()
+        readproj = True
+    except OSError:              #I think this means there is no such file
+        with open('project.txt','w') as f:
+            f.write('000000')
+            Project = "000000"
+
+print (f'Project No: {Project}')
+
+# Services and Characteristics
+device_info = aioble.Service(_DEVICE_INFO_UUID)
+project_info = aioble.Service(_PROJECT_UUID)
+uart_service = aioble.Service(_UART_UUID)
+battery_info = aioble.Service(_BATTERY_UUID)
+
+proj_characteristic = aioble.Characteristic(project_info, _PROJ_NUM_UUID, read=True, write=True, capture=True, initial=Project)
+keepalive_characteristic = aioble.Characteristic(project_info, _PROJ_KEEPALIVE_UUID, write=True, capture=True, initial="Not OK")
+tx_characteristic = aioble.Characteristic(uart_service, _TX_UUID, read=True, notify=True)
+rx_characteristic = aioble.Characteristic(uart_service, _RX_UUID, write=True, notify=True, capture=True)
+batt_level = aioble.Characteristic(battery_info, _BATTERY, read=True, notify=True)
+
+aioble.Characteristic(device_info, bluetooth.UUID(MANUFACTURER_ID), read=True, initial=Company)
+aioble.Characteristic(device_info, bluetooth.UUID(MODEL_NUMBER_ID), read=True, initial=Model)
+aioble.Characteristic(device_info, bluetooth.UUID(SERIAL_NUMBER_ID), read=True, initial=uid())
+aioble.Characteristic(device_info, bluetooth.UUID(HARDWARE_REVISION_ID), read=True, initial=Hardware) #sys.version)
+aioble.Characteristic(device_info, bluetooth.UUID(BLE_VERSION_ID), read=True, initial=Software)
+
+#Register services
+aioble.register_services(uart_service, device_info, project_info, battery_info)
+
+#Global Variables
+connected = False
+connection = None
+
+# Tasks
 async def peripheral_task():
-    """ Task to handle peripheral """
     print('peripheral task started')
     global connected, connection, message, Project, count
     while True:
@@ -164,12 +137,11 @@ async def peripheral_task():
             
             while connected:
                 if connection.is_connected():
-                    count = count + 1
-                    if count > 3 : #COUNT_MAX:
+                    count += 1
+                    if count > 10 : #COUNT_MAX:
                         print("It's dead or it's not GameChanger at the other end!")
                         connection = None
                         connected = False
-                        alive = False
                         count = 0 #reset for the next connection
                     else:                 
                         message = "AusSport P" + Project
@@ -178,9 +150,9 @@ async def peripheral_task():
                         await asyncio.sleep_ms(5_000)
                 else:
                     print("Disconnected")
-                    connected = None  #
+                    connected = False
             #await (not connected)
-                    print("disconnected or dead")
+                print("disconnected or dead")
 
 async def keepalive_task():
     global connected, connection, count
@@ -188,19 +160,16 @@ async def keepalive_task():
 
     count = 0
     while True:
-        if connected == True:
-           # print("Connected in RX")
+        if connected:
             alive = True
         else:
-           # print("Not Connected")
             alive = False
             count = 0
             await asyncio.sleep_ms(100)
             continue
     
-        if alive == True:
-             
-            if keepalive_characteristic != None:
+        if alive:        
+            if keepalive_characteristic:
                 try:
                     print("Waiting for Keep Alive...")
                     connection, keepalive = await keepalive_characteristic.written()
@@ -210,148 +179,81 @@ async def keepalive_task():
                         print(ret_char)
                     await asyncio.sleep_ms(5000)
                         
-                except TypeError:
+                except (TypeError, asyncio.TimeoutError, asyncio.GattError):
                     print(f'something went wrong; remote disconnected?')
                     connected = False
                     alive = False
-                    return
-                except asyncio.TimeoutError:
-                    print(f'something went wrong; timeout error?')
-                    connected = False
-                    alive = False
-                    return
-                except asyncio.GattError:
-                    print(f'something went wrong; Gatt error - did the remote die?')
-                    connected = False
-                    alive = False
-                    return
-
 
 async def proj_task():
     global connected, connection, Project
     print('proj task started')
-    global read_char
-    read_char = False
     while True:
-        if connected == True:
-           # print("Connected in RX")
+        if connected:
             alive = True
         else:
-           # print("Not Connected")
             alive = False
             await asyncio.sleep_ms(100)
             continue
     
-        if alive == True:
+        if alive:
              
-            if proj_characteristic != None:
+            if proj_characteristic:
                 try:
-                    #command = rx_characteristic(..., capture=True)
                     print("Waiting for any change in Project No...")
-                    read_char = False
-                    connection, rec_val = await proj_characteristic.written()  #rx_characteristic.write()
+                    connection, rec_val = await proj_characteristic.written()
                     Project = rec_val.decode('ascii')
                     update_str = "Received New Project: " + Project
                     print (update_str)
                     tx_characteristic.write(update_str.encode('ascii'), send_update=True)
-                    read_char = True
-                    f1 = open('project.txt','w')
-                    f1.write(Project)
-                    f1.close()
+                    with open('project.txt','w') as f:
+                        f.write(Project)
                     await asyncio.sleep_ms(50)
                     
-                        
-                except TypeError:
+                except (TypeError, asyncio.TimeoutError, asyncio.GattError):
                     print(f'something went wrong; remote disconnected?')
                     connected = False
                     alive = False
-                    return
-                except asyncio.TimeoutError:
-                    print(f'something went wrong; timeout error?')
-                    connected = False
-                    alive = False
-                    return
-                except asyncio.GattError:
-                    print(f'something went wrong; Gatt error - did the remote die?')
-                    connected = False
-                    alive = False
-                    return
-            await asyncio.sleep_ms(1)
-
-
 
 async def rx_task():
     global connected, connection
     print('rx task started')
-    global read_char
-    read_char = False
     while True:
-        if connected == True:
-           # print("Connected in RX")
+        if connected:
             alive = True
         else:
-            print("Not Connected")
             alive = False
             await asyncio.sleep_ms(100)
             continue
     
-        if alive == True:
-             
-            if rx_characteristic != None:
+        if alive:             
+            if rx_characteristic:
                 try:
-                    #command = rx_characteristic(..., capture=True)
                     print("Waiting for RX chars...")
-                    read_char = False
                     connection, rec_val = await rx_characteristic.written()  #rx_characteristic.write()
                     Message = rec_val.decode('ascii')
                     print (f"Received: {Message}")
-                    read_char = True
                     code = lora.send_transparent_message(Message)
                     print(f"Send Radio message: {Message}", ResponseStatusCode.get_description(code))
                     tx_characteristic.write(Message.encode('ascii'), send_update=True)
                     await asyncio.sleep_ms(50)
                     
-                        
-                except TypeError:
+                except (TypeError, asyncio.TimeoutError, asyncio.GattError):
                     print(f'something went wrong; remote disconnected?')
                     connected = False
                     alive = False
-                    return
-                except asyncio.TimeoutError:
-                    print(f'something went wrong; timeout error?')
-                    connected = False
-                    alive = False
-                    return
-                except asyncio.GattError:
-                    print(f'something went wrong; Gatt error - did the remote die?')
-                    connected = False
-                    alive = False
-                    return
-            await asyncio.sleep_ms(1)
             
-    # Helper to encode the voltage characteristic encoding (sint16).
-def _encode_voltage(vc):
-    return struct.pack("<h", int(vc))
-
-
 async def read_voltage():
     print ('read voltage started')
     global connected
-    
-    toggle = True
     while True:
         charging = Pin('WL_GPIO2', Pin.IN)          # reading GP24 tells us whether or not USB power is connected
         conversion_factor = 3 * 3.3 / 65535
-
         full_battery = 4.2                  # these are our reference voltages for a full/empty battery, in volts
         empty_battery = 2.8                 # the values could vary by battery size/manufacturer so you might need to adjust them
         voltage = measure_vsys() * conversion_factor
         # convert the raw ADC read into a voltage, and then a percentage
         percentage = 100 * ((voltage - empty_battery) / (full_battery - empty_battery))
-        if percentage > 100:
-            percentage = 100.00
-
-
+        percentage = min(max(percentage, 0), 100)
 #        print(f'Battery percentage remaining {percentage}')
         percent = _encode_voltage(percentage)
         batt_level.write(percent, send_update=True)
@@ -363,25 +265,17 @@ async def read_voltage():
         await asyncio.sleep_ms(5_000)
 
 
-
 async def blink_task():
-    """ Task to blink LED """
     print ('blink task started')
     global connected
     toggle = True
     while True:
         led.value(toggle)
         toggle = not toggle
-        blink = 1000
-        if connected:
-            blink = 1000
-        else:
-           # print ("Not Connected")
-            blink = 250
+        blink = 1000 if connected else 250
         await asyncio.sleep_ms(blink)
 
 async def main():
-
     tasks = [
         asyncio.create_task(peripheral_task()),
         asyncio.create_task(blink_task()),
